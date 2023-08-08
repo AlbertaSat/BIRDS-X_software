7a1cd556734bed74eba5c546336902f7a3555234
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-08-07 20:46:25 -0600

    Revert custom board, configure serial

--- JASPER_BIRDS-X_SX1276_PIO/src/main.cpp
+++ JASPER_BIRDS-X_SX1276_PIO/src/main.cpp
@@ -1,28 +1,37 @@
 #include <Arduino.h>
 
-#define DebugPort Serial1
+HardwareSerial BossSerial(USART1);
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("INFO: Booting JASPER SX1276 Board (Rev2) with PIO Firmware");
 
-  // LEDs in output mode
-  pinMode(PC6, OUTPUT);
-  pinMode(PA11, OUTPUT);
+	BossSerial.setRx(PB7);
+	BossSerial.setTx(PB6);
+	BossSerial.begin(115200);
+	BossSerial.println("INFO: Booting JASPER SX1276 Board (Rev2) with PIO Firmware");
+
+	// Debug LEDs in output mode
+	pinMode(PC6, OUTPUT);
+	pinMode(PA11, OUTPUT);
+
+
+	uint64_t superloop_counter = 0;
+	while (1) {
+		BossSerial.printf("INFO: Starting loop() (superloop_counter=%lu)\n", superloop_counter++);
+
+		// write PC6 and PA11 to blink
+		digitalWrite(LED_BUILTIN, HIGH);
+		digitalWrite(PA11, HIGH);
+
+		delay(500);
+
+		digitalWrite(LED_BUILTIN, LOW);
+		digitalWrite(PA11, LOW);
+		delay(1000);
+	}
 
 }
 
 void loop() {
-  Serial.println("INFO: Starting loop() XXXX;");
-
-  // write PC6 and PA11 to blink
-  digitalWrite(PC6, HIGH);
-  digitalWrite(PA11, HIGH);
-
-  delay(500);
-
-  digitalWrite(PC6, LOW);
-  digitalWrite(PA11, LOW);
-  delay(1000);
-
+	BossSerial.println("ERROR: Please reboot the board. Superloop broke.");
+	delay(1000);
 }
