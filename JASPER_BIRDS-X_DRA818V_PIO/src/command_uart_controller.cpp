acc2d003948b79cd10bf573a320334c4697ff7e0
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-08-10 20:09:41 -0600

    Attempt

--- JASPER_BIRDS-X_DRA818V_PIO/src/command_uart_controller.cpp
+++ JASPER_BIRDS-X_DRA818V_PIO/src/command_uart_controller.cpp
@@ -1,13 +1,15 @@
 #include <HardwareSerial.h>
 #include <Arduino.h>
+
 #include "command_uart_controller.h"
+#include "pin_defs.h"
 
 HardwareSerial BossSerial(USART1);
 
 void init_boss_serial() {
 	
-	BossSerial.setRx(PB7);
-	BossSerial.setTx(PB6);
+	BossSerial.setRx(PIN_UART_BOSS_TO_JASPER);
+	BossSerial.setTx(PIN_UART_JASPER_TO_BOSS);
 	BossSerial.begin(115200);
 
 	BossSerial.println("\n\nINFO: Booting JASPER SX1276 Board (Rev2) with PIO Firmware");
--- JASPER_BIRDS-X_DRA818V_PIO/src/main_loop_controller.cpp
+++ JASPER_BIRDS-X_DRA818V_PIO/src/main_loop_controller.cpp
@@ -1,18 +1,49 @@
-#include "main_loop_controller.h"
+#include <Arduino.h>
 
+#include "main_loop_controller.h"
+#include "pin_defs.h"
 #include "command_uart_controller.h"
 #include "rf_controller.h"
+
+HardwareSerial TncSerial(LPUART1);
 
 void run_once_at_boot() {
 	init_boss_serial();
 
-	init_sx1276();
+	pinMode(PIN_ONBOARD_LED, OUTPUT);
+
+	// TODO: remove this
+	// set UART to high-Z so we can use a regular adapter on it
+	// pinMode(PIN_UART_TNC_TO_JASPER, INPUT);
+	// pinMode(PIN_UART_JASPER_TO_TNC, INPUT);
+
+	TncSerial.setRx(PIN_UART_TNC_TO_JASPER);
+	TncSerial.setTx(PIN_UART_JASPER_TO_TNC);
+	TncSerial.begin(38400); // source: https://nbviewer.org/github/mobilinkd/NucleoTNC/blob/master/Build/NucleoTNC.ipynb#Programming:~:text=is%20now%20operational!-,Operation,-Before%20we%20begin
 
 }
 
 
 void main_loop_single_iteration() {
-	do_test_sx1276();
+
+	BossSerial.println("INFO: main_loop_single_iteration()");
+
+	// do serial passthrough
+	// TODO: revert this eventually
+	while (BossSerial.available()) {
+		TncSerial.write(BossSerial.read());
+	}
+	if (TncSerial.available()) {
+		BossSerial.print("INFO: TNC serial incoming: ");
+		while (TncSerial.available()) {
+			BossSerial.write(TncSerial.read());
+		}
+	}
+
+	digitalWrite(PIN_ONBOARD_LED, HIGH);
+	delay(10);
+	digitalWrite(PIN_ONBOARD_LED, LOW);
+	delay(100);
 	
 }
 
