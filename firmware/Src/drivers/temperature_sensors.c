07ad41c9840def9b1e2debffd6f850d7a525db40
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-09-05 00:36:33 -0600

    Try to fix up support for negative Celsius temps

--- firmware/Src/drivers/temperature_sensors.c
+++ firmware/Src/drivers/temperature_sensors.c
@@ -27,21 +27,18 @@ uint16_t get_external_temperature_k(uint8_t sensor_index) {
 		&hi2c2, read_reg, 0x00, I2C_MEMADD_SIZE_8BIT, read_data, 2, 1000
 	);
 
-	const uint8_t resolution = 9; // default, 0.5C sensitivity, fastest reads
-	// return ((read_data[0] >> (16 - resolution)) * 1000) >> (resolution - 8);
-
-	// return read_data[1]; // TODO: include the other byte read
-
+	// const uint8_t resolution = 9; // default, 0.5C sensitivity, fastest reads
+	
+	uint8_t sign_bit = read_data[0] & (1 << 7); // 0=positive, 1=negative; 2's complement
 	uint16_t temp_as_9_bits = (read_data[0] << 1 | read_data[1] >> 7);
 	uint16_t temp_unsigned_c = temp_as_9_bits >> 1; // divide by 2, because 0.5C per bit
+	// TODO: maybe do sign extension here?
+	// FIXME: this probably doesn't work with negative values
 	
 	// execute two-complement conversion
-	int16_t temp_signed_c;
-	if (temp_unsigned_c & 0x0100) {
-		temp_signed_c = -1 * ((~temp_unsigned_c + 1) & 0x01FF);
-	} else {
-		temp_signed_c = temp_unsigned_c;
-	}
+	int8_t sign = (sign_bit == 0) ? 1 : -1; // Determine the sign
+	int16_t absolute_value = (temp_unsigned_c & ~(1 << 15)); // Calculate absolute value
+	int16_t temp_signed_c = sign * absolute_value;
 
 	// convert to kelvin
 	uint16_t temp_k = temp_signed_c + 273;
