f3b89e187cb16586f4d04ecadfcaf4fef2cbb353
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-11-18 18:12:13 -0700

    Enable RFET_EN_OUT_PIN as GPIO_OUTPUT pin

--- firmware/Inc/main.h
+++ firmware/Inc/main.h
@@ -75,6 +75,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define RFET_EN_OUT_PIN_Pin GPIO_PIN_12
+#define RFET_EN_OUT_PIN_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
